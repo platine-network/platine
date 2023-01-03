c3f29a05205c50fa258846d1532006fb2f29cccc
Tony NGUEREZA <nguerezatony@gmail.com>
2023-01-03 20:39:04 +0100

    Fix issue with epoch generation

--- x/epoch/module.go
+++ x/epoch/module.go
@@ -143,7 +143,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
-func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
+func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
 	am.keeper.BeginBlocker(ctx)
 }
 
