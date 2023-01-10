c864c63d5d245324493c2e2e5351b1d9d8004ad6
NGUEREZA Tony <nguerezatony@gmail.com>
2023-01-10 20:22:21 +0100

    Minor change in docker config files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,14 @@
 # o -> sets the exit code to the rightmost command 
 set -euo pipefail
 
+SED_EXT=''
+# sed in macos requires extra argument
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    SED_EXT=''
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_EXT='.orig'
+fi
+
 # within the container, $HOME=/home/platined
 PLATINED_ROOT_DIR="$HOME/.platine"
 PLATINED_BOOT_FILE="$HOME/.boot"
@@ -30,6 +38,24 @@ if [ ! -f "$PLATINED_BOOT_FILE" ];then
         echo "No genesis file passed. Skipping and retaining existing genesis."
     fi
 
+    # Check if a app.toml file has been passed in config
+    if [ -f "/app_toml" ]
+    then
+        echo "app.toml file passed. Adding/replacing current app.toml file."
+        cp /app_toml "${PLATINED_ROOT_DIR}/config/app.toml"
+    else
+        echo "No app.toml file passed. Skipping and retaining existing app.toml."
+    fi
+
+    # Check if a config.toml file has been passed in config
+    if [ -f "/config_toml" ]
+    then
+        echo "config.toml file passed. Adding/replacing current config.toml file."
+        cp /config_toml "${PLATINED_ROOT_DIR}/config/config.toml"
+    else
+        echo "No config.toml file passed. Skipping and retaining existing config.toml."
+    fi
+
     # Check if a seeds file has been passed in config
     if [ -f "/seeds" ]
     then
@@ -37,6 +63,11 @@ if [ ! -f "$PLATINED_BOOT_FILE" ];then
         cp /seeds "${PLATINED_ROOT_DIR}/config/seeds.txt"
         PLATINED_P2P_SEEDS="$(cat "${PLATINED_ROOT_DIR}/config/seeds.txt")"
         export PLATINED_P2P_SEEDS
+        if [ -f "${PLATINED_ROOT_DIR}/config/config.toml" ]
+        then
+            echo "Adding persistence peers to config.toml."
+            sed -i $SED_EXT "s/persistent_peers = \"\"/persistent_peers = \"${PLATINED_P2P_SEEDS}\"/g" "${PLATINED_ROOT_DIR}/config/config.toml"
+        fi
     else
         echo "No seeds file passed. Skipping and retaining existing seeds."
     fi
